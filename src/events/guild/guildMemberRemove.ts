58cdc053e9da1af0ccdec0d95d2957ca96c9dad0
CalSQ <caffrey.squad@gmail.com>
2024-01-05 16:54:39 +0000

    feat: Logging for member leaves

---
+++ src/events/guild/guildMemberRemove.ts
@@ -0,0 +1,20 @@
+import { EmbedBuilder, Events } from "discord.js";
+import { Guild } from "../../models";
+import { event } from "../../interfaces";
+
+export default event(Events.GuildMemberRemove, false, async ({ client, log }, member) => {
+    const guildData = await Guild.getById(member.guild.id);
+    const channelId = guildData.values.options.memberLog;
+    if (!channelId) return;
+    const channel = client.channels.cache.get(channelId) ?? await client.channels.fetch(channelId);
+    if (!channel?.isTextBased()) return;
+
+    const Embed = new EmbedBuilder()
+        .setAuthor({ name: member.user.username, iconURL: member.displayAvatarURL() })
+        .setDescription(`${member} left the server!\nThere are now **${member.guild.memberCount}** members in the server!`)
+        .setFooter({text: member.guild.id})
+        .setColor(client.config.colors.memberLog.leave ?? client.config.colors.default)
+        .setTimestamp(member.joinedTimestamp);
+
+    channel.send({ embeds: [Embed] });
+})
