fe3f861be39f52811421f032a2e0083815ab37d1
CalSQ <caffrey.squad@gmail.com>
2024-01-06 21:09:23 +0000

    temp: Command to fill chat with empty space

--- src/commands/slash/developer/test.ts
+++ src/commands/slash/developer/test.ts
@@ -5,6 +5,19 @@ const build = new SlashCommandBuilder()
     .setName('test')
     .setDescription('Developer commands')
 
-export default command<ChatInputCommandInteraction>(build, async ({ interaction }) => {
-    interaction.reply("Working")
+export default command<ChatInputCommandInteraction>(build, async ({ client, interaction }) => {
+
+    const logChannel = client.channels.cache.get("1138924763155279884");
+    if (!logChannel?.isTextBased()) return interaction.reply("Could not complete task sir.");
+
+    const lastMessage = logChannel.lastMessage;
+    if (!lastMessage) return interaction.reply("Could not complete task sir.");
+
+    const embed = lastMessage.embeds[0];
+    const copiedEmbed = EmbedBuilder.from(embed);
+
+    await logChannel.send(`* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n*\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n* *\n`);
+    await logChannel.send({ embeds: [copiedEmbed] });
+
+    interaction.reply("Done!");
 })
