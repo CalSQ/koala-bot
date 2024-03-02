4e760c72cd60c6a72b36ba3877ca95efbb1a790e
CalSQ <caffrey.squad@gmail.com>
2024-03-02 04:36:31 +0000

    feat: webhook reply to mention

--- bot/src/events/guild/messageCreate.ts
+++ bot/src/events/guild/messageCreate.ts
@@ -1,20 +1,52 @@
-import { Events } from "discord.js";
-import { event } from "../../interfaces";
+import { BaseGuildTextChannel, EmbedBuilder, Events } from "discord.js"
+import { event } from "../../interfaces"
 
-export default event(Events.MessageCreate, false, async ({ client }, message) => {
-    if (message.author.id !== client.config.info.developerId) return;
-    const msgArguments = message.content.toLowerCase().split(" ")
+export default event(
+  Events.MessageCreate,
+  false,
+  async ({ client }, message) => {
+    if (
+      message.mentions.has(client.config.info.developerId) &&
+      message.channel instanceof BaseGuildTextChannel &&
+      message.guildId === client.config.info.developerGuild &&
+      !message.author.bot
+    ) {
+      const webhooks = await message.channel.fetchWebhooks()
+      const owner = await client.users.fetch(client.config.info.developerId)
+      let webhook = webhooks.find((hook) => {
+        hook.name === "Mention"
+      })
+      if (!webhook) {
+        webhook = await message.channel.createWebhook({
+          name: owner.username,
+          avatar: owner.displayAvatarURL(),
+          reason: "To reply to ping for me lol",
+        })
+      }
 
-    //////////////////////////////////////////////////////////////////////////
-
-    switch(msgArguments[0]) {
-
-        case (".test"): {
-            message.reply({content: "Working!"})
-            break;
-        }
-
+      await webhook.send({ content: `${message.author} hi` })
+      const embed = new EmbedBuilder()
+        .setAuthor({
+          name: message.author.username,
+          iconURL: message.author.displayAvatarURL(),
+        })
+        .setDescription(`${message.content}\n${message.url}`)
+      await owner.send({ embeds: [embed] })
     }
 
-    //////////////////////////////////////////////////////////////////////////
-})
+    if (message.author.id === client.config.info.developerId) {
+      const msgArguments = message.content.toLowerCase().split(" ")
+
+      //////////////////////////////////////////////////////////////////////////
+
+      switch (msgArguments[0]) {
+        case ".test": {
+          message.reply({ content: "Working!" })
+          break
+        }
+      }
+
+      //////////////////////////////////////////////////////////////////////////
+    }
+  }
+)
