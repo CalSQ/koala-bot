1e3a5e946a1b3cc4dcc0466b348550ea1f857d3f
CS <130398780+CalSQ@users.noreply.github.com>
2024-04-11 17:19:59 +0100

    Update messageCreate.ts

--- bot/src/events/guild/messageCreate.ts
+++ bot/src/events/guild/messageCreate.ts
@@ -5,34 +5,34 @@ export default event(
   Events.MessageCreate,
   false,
   async ({ client }, message) => {
-    if (
-      message.mentions.has(client.config.info.developerId) &&
-      message.channel instanceof BaseGuildTextChannel &&
-      message.guildId === client.config.info.developerGuild &&
-      !message.author.bot
-    ) {
-      const webhooks = await message.channel.fetchWebhooks()
-      const owner = await client.users.fetch(client.config.info.developerId)
-      let webhook = webhooks.find((hook) => {
-        hook.name === "Mention"
-      })
-      if (!webhook) {
-        webhook = await message.channel.createWebhook({
-          name: owner.username,
-          avatar: owner.displayAvatarURL(),
-          reason: "To reply to ping for me lol",
-        })
-      }
+    // if (
+    //   message.mentions.has(client.config.info.developerId) &&
+    //   message.channel instanceof BaseGuildTextChannel &&
+    //   message.guildId === client.config.info.developerGuild &&
+    //   !message.author.bot
+    // ) {
+    //   const webhooks = await message.channel.fetchWebhooks()
+    //   const owner = await client.users.fetch(client.config.info.developerId)
+    //   let webhook = webhooks.find((hook) => {
+    //     hook.name === "Mention"
+    //   })
+    //   if (!webhook) {
+    //     webhook = await message.channel.createWebhook({
+    //       name: owner.username,
+    //       avatar: owner.displayAvatarURL(),
+    //       reason: "To reply to ping for me lol",
+    //     })
+    //   }
 
-      await webhook.send({ content: `${message.author} hi` })
-      const embed = new EmbedBuilder()
-        .setAuthor({
-          name: message.author.username,
-          iconURL: message.author.displayAvatarURL(),
-        })
-        .setDescription(`${message.content}\n${message.url}`)
-      await owner.send({ embeds: [embed] })
-    }
+    //   await webhook.send({ content: `${message.author} hi` })
+    //   const embed = new EmbedBuilder()
+    //     .setAuthor({
+    //       name: message.author.username,
+    //       iconURL: message.author.displayAvatarURL(),
+    //     })
+    //     .setDescription(`${message.content}\n${message.url}`)
+    //   await owner.send({ embeds: [embed] })
+    // }
 
     if (message.author.id === client.config.info.developerId) {
       const msgArguments = message.content.toLowerCase().split(" ")
