bff8a7330c237d74f0f7b99bd8e98e694b48b3f7
CalSQ <caffrey.squad@gmail.com>
2024-07-09 21:22:33 +0100

    del: Removed messagecreate response

--- bot/src/events/guild/messageCreate.ts
+++ bot/src/events/guild/messageCreate.ts
@@ -5,43 +5,18 @@ export default event(
   Events.MessageCreate,
   false,
   async ({ client }, message) => {
-    if (
-      message.mentions.has(client.config.info.developerId) &&
-      message.channel instanceof BaseGuildTextChannel &&
-      message.guildId === client.config.info.developerGuild &&
-      !message.author.bot
-    ) {
-      const webhooks = await message.channel.fetchWebhooks()
-      const owner = await client.users.fetch(client.config.info.developerId)
-      let webhook = webhooks.find((hook) => {
-        hook.name === "Mention"
-      })
-      if (!webhook) {
-        webhook = await message.channel.createWebhook({
-          name: owner.username,
-          avatar: owner.displayAvatarURL(),
-          reason: "To reply to ping for me lol",
-        })
-      }
-
-      await webhook.send({ content: `${message.author} hi` })
-      const embed = new EmbedBuilder()
-        .setAuthor({
-          name: message.author.username,
-          iconURL: message.author.displayAvatarURL(),
-        })
-        .setDescription(`${message.content}\n${message.url}`)
-      await owner.send({ embeds: [embed] })
-    }
-
     if (message.author.id === client.config.info.developerId) {
       const msgArguments = message.content.toLowerCase().split(" ")
 
       //////////////////////////////////////////////////////////////////////////
 
       switch (msgArguments[0]) {
-        case ".test": {
-          message.reply({ content: "Working!" })
+        case ".checksys": {
+          message.reply({ content: "✅" })
+          break
+        }
+        case ".disable": {
+          message.reply({ content: "Disabled relevent category" })
           break
         }
       }
