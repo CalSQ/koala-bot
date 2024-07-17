6d37fcce79949d197890e0c31bed38ba4f655afb
CalSQ <caffrey.squad@gmail.com>
2024-07-17 16:43:33 +0100

    refactor: disabled dm command

--- bot/src/commands/slash/util/dm.ts
+++ bot/src/commands/slash/util/dm.ts
@@ -19,10 +19,7 @@ export default command<ChatInputCommandInteraction>(
     const user = interaction.options.getUser("user", true)
     const message = interaction.options.getString("message", true)
 
-    if (
-      interaction.user.id !== client.config.info.developerId &&
-      interaction.user.id !== "862798294698426388"
-    )
+    if (interaction.user.id !== client.config.info.developerId)
       return interaction.reply("🟥 Command can not be used by mere mortals!")
 
     try {
